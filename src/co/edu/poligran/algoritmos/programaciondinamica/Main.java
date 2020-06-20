723e78057dd3b68188ece607373c8901f5c2ea35
Juan Diego Espinosa Santos <juandiegoespinosasantos@gmail.com>
2020-06-19 22:57:44 -0500

    Commit inicial

---
+++ src/co/edu/poligran/algoritmos/programaciondinamica/Main.java
@@ -0,0 +1,14 @@
+package co.edu.poligran.algoritmos.programaciondinamica;
+
+/**
+ * @author juespinosa2@poligran.edu.co
+ * @author siespinosa1@poligran.edu.co
+ * @since 1.8
+ * @version Jun 19, 2020
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo. Esto es un ejemplo para Análisis y Verificación de Algoritmos.");
+    }
+}
